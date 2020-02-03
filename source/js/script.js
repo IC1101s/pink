d943ca749c79ee3c5c37dcfb7f7f04572fa39200
IC1101 <stas-s2000@mail.ru>
2020-02-03 15:40:40 +0300

    Добавил комментарий

--- source/js/script.js
+++ source/js/script.js
@@ -12,3 +12,14 @@ mainToggle.addEventListener("click", function () {
 		mainNav.classList.remove("main-nav--opened");
 	}
 });
+
+// var sliderToggle = document.querySelectorAll(".slider__toggle");
+
+// sliderToggle.addEventListener("click", function () {
+	
+// 	if (sliderToggle.classList.contains("slider__toggle--active")) {
+// 		sliderToggle.classList.remove("slider__toggle--active");
+// 	} else {
+// 		sliderToggle.classList.add("slider__toggle--active");
+// 	}
+// });
